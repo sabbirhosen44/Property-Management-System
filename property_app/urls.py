b57526bb4f389f67cff75744df43493fd2d1c638
sabbir <sabbirhosen.pstu@gmail.com>
2026-06-22 19:02:51 +0600

    feat: add property list and detail page styling
    - Added property_list.css and property_detail.css files.
    - Restructured base.html for clean template inheritance.
    - Updated property templates to use the new layout.

--- property_app/urls.py
+++ property_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import HomeView
-
+from .views import HomeView, PropertyListView, PropertyDetailView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("properties/", PropertyListView.as_view(), name="property_list"),
+    path(
+        "properties/<slug:slug>/", PropertyDetailView.as_view(), name="property_detail"
+    ),
 ]
--- property_app/views.py
+++ property_app/views.py
@@ -6,7 +6,20 @@ from .models import Property
 
 
 class HomeView(TemplateView):
+
     template_name = "property_app/home.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context["featured_properties"] = Property.objects.select_related(
+            "location"
+        ).all()[:6]
+
+        context["total_properties"] = Property.objects.count()
+
+        return context
 
 
 class PropertyListView(ListView):
